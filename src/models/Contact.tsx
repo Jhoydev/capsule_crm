7cd484f012b8fd48246208c0e2ac607cdbdd3c13
Dani <dsarabia@idealista.com>
2024-06-06 08:13:48 +0200

    agregada interfaz completa de contacto, dejada comentada.

--- src/models/Contact.tsx
+++ src/models/Contact.tsx
@@ -7,6 +7,27 @@ export interface Contact {
     email: string;
 }
 
+// export interface Contact {
+//     id: number;
+//     first_name: string;
+//     last_name: string;
+//     nif: string;
+//     email: string;
+//     alternate_email: string;
+//     phone: string;
+//     mobile: string;
+//     avatar_url: string;
+//     birthday: string;
+//     contact_medium: string;
+//     language: string;
+//     notes: string;
+//     rgpd: string;
+//     profession: string;
+//     company: string;
+//     gender: string;
+//     user_id: number;
+// }
+
 export interface ApiResponseContact {
     message: string;
     contact: Contact;
