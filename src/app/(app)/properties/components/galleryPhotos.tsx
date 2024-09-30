ecc5d7c7357a5ee65f923dbe9ca432abc525d089
Dani <dsarabia@idealista.com>
2024-09-30 19:02:46 +0200

    ajustes componente galeria fotos.

---
+++ src/app/(app)/properties/components/galleryPhotos.tsx
@@ -0,0 +1,52 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import {Property} from "@/types/property.types";
+import React from "react";
+import '../styles/mySwiper.css';
+import GallerySwiper from "@/app/(app)/properties/components/gallerySwiper";
+
+interface PropertyProps {
+    property: Property
+}
+
+const GalleryPhotos: React.FC<PropertyProps> = ({ property }) => {
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+            <div className="md:col-span-3">
+                <div className="h-[400px]">
+                    <GallerySwiper property={property}/>
+                </div>
+            </div>
+            <div className="md:flex flex-col justify-between md:col-span-1 hidden ">
+                <div className="relative w-full h-[190px] rounded-md">
+                    <img className="rounded-md w-full h-full object-cover" alt="Imagen"
+                         src="https://fotos15.inmovilla.com/554/22034174/4-3.jpg"/>
+                    <div className="absolute inset-0 bg-gray-500 bg-opacity-50 rounded-md"></div>
+                </div>
+                <div className="relative w-full h-[190px] rounded-md">
+                    <img className="rounded-md w-full h-full object-cover" alt="Imagen"
+                         src="https://fotos15.inmovilla.com/554/22034174/4-4.jpg"/>
+                    <div className="absolute inset-0 bg-gray-500 bg-opacity-50 rounded-md"></div>
+                    <div className="absolute inset-0 flex items-center justify-center cursor-pointer">
+                        <span
+                            className="bg-white bg-opacity-70 text-gray-800 rounded-lg px-4 py-2 text-sm font-medium">
+                            +4 Photos
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default GalleryPhotos;
+
+
+
--- src/app/(app)/properties/components/propertyView.tsx
+++ src/app/(app)/properties/components/propertyView.tsx
@@ -6,14 +6,10 @@ import { FaEdit } from "react-icons/fa";
 import { Property } from "@/types/property.types";
 import TabProperty from "@/app/(app)/properties/components/tab";
 import AgentDetails from "@/app/(app)/properties/components/agentDetails";
-import GallerySwiper from "@/app/(app)/properties/components/gallerySwiper";
 import PropertyDetails from "@/app/(app)/properties/components/propertyDetails";
-import LocationDetails from "@/app/(app)/properties/components/locationDetails";
 import ClientPropertyDetails from "@/app/(app)/properties/components/clientPropertyDetails";
 import dynamic from 'next/dynamic';
-import {PiBathtubLight} from "react-icons/pi";
-import {IoBedOutline} from "react-icons/io5";
-import {BiSolidCarGarage} from "react-icons/bi";
+import GalleryPhotos from "@/app/(app)/properties/components/galleryPhotos";
 
 //Esto es útil para componentes que dependen de objetos o propiedades disponibles solo en el navegador, como window o document, que no existen en el entorno de servidor.
 //porl o que con la siguiente intruccion le decimos que cargue el componente mapa de forma dinamica y le indicamos con el ssr false que no lo haga en el lado del servidor.
@@ -49,36 +45,11 @@ const propertyView: React.FC<propertyViewProps> = ({ editFunction, data }) => {
                     </button>
                 </div>
             </div>
-            <div className="grid grid-rows-2 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-4 m-5 p-5 grid-rows-[auto,200px]">
+            <div className="grid grid-rows-2 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-4 m-5 p-5 grid-rows-[auto,auto]">
                 <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-7 md:col-span-1 gap-4">
                     <div className="md:col-span-5 border p-5 shadow rounded-md">
-                        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                            <div className="md:col-span-3">
-                                <div className="h-[400px]">
-                                    <GallerySwiper property={data}/>
-                                </div>
-                            </div>
-                            <div className="md:flex flex-col justify-between md:col-span-1 hidden ">
-                                <div className="relative w-full h-[190px] rounded-md">
-                                    <img className="rounded-md w-full h-full object-cover" alt="Imagen"
-                                         src="https://fotos15.inmovilla.com/554/22034174/4-3.jpg"/>
-                                    <div className="absolute inset-0 bg-gray-500 bg-opacity-50 rounded-md"></div>
-                                </div>
-                                <div className="relative w-full h-[190px] rounded-md">
-                                    <img className="rounded-md w-full h-full object-cover" alt="Imagen"
-                                         src="https://fotos15.inmovilla.com/554/22034174/4-4.jpg"/>
-                                    <div className="absolute inset-0 bg-gray-500 bg-opacity-50 rounded-md"></div>
-                                    <div className="absolute inset-0 flex items-center justify-center cursor-pointer">
-                                        <span className="bg-white bg-opacity-70 text-gray-800 rounded-lg px-4 py-2 text-sm font-medium">
-                                            +4 Photos
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <PropertyDetails property={data}/>
-                        </div>
+                        <GalleryPhotos property={data}/>
+                        <PropertyDetails property={data}/>
                     </div>
                     <div className="md:col-span-2 flex flex-col">
                         <AgentDetails/>
