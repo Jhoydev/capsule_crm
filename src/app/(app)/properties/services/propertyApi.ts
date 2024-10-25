5bc6a8b6141fc10f90513f327ccff5c84780ff89
Dani <dsarabia@idealista.com>
2024-10-25 11:48:53 +0200

    ajuste para obtener imagnes de las propiedades

--- src/app/(app)/properties/services/propertyApi.ts
+++ src/app/(app)/properties/services/propertyApi.ts
@@ -5,7 +5,7 @@ import { Property, ApiResponseProperty } from '@/types/property.types';
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost/api';
 
 export const getProperty = async (id: number): Promise<Property> => {
-    const { data } = await axios.get<Property>(`${API_BASE_URL}/properties/${id}`);
+    const { data } = await axios.get<Property>(`${API_BASE_URL}/properties/${id}?includes=image`);
     return data;
 };
 
