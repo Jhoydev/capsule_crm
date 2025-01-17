ba8226a855c365203288ff9765ac57171a72b9b5
Jhoydev <jhoseph.dev@gmail.com>
2025-01-17 14:36:17 +0100

    fix(contact table): Align action button to right

--- src/app/(app)/contacts/components/contacts-table/data-table-row-actions.tsx
+++ src/app/(app)/contacts/components/contacts-table/data-table-row-actions.tsx
@@ -13,31 +13,10 @@ export function DataTableRowActions<TData>({ row, }: DataTableRowActionsProps<TD
     const router = useRouter();
     const id = row.getValue('id');
     return (
-        <Button variant="outline" size="icon" onClick={() => router.push(`/contacts/${id}`)}>
-            <ChevronRight />
-        </Button>
-
-    // <DropdownMenu>
-    //     <DropdownMenuTrigger asChild>
-    //         <Button
-    //             variant="ghost"
-    //             className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-    //         >
-    //             <DotsHorizontalIcon className="h-4 w-4" />
-    //             <span className="sr-only">Open menu</span>
-    //         </Button>
-    //     </DropdownMenuTrigger>
-    //     <DropdownMenuContent align="end" className="w-[160px]">
-    //         <Link href={`/contacts/${row.getValue('id')}`}>
-    //             <DropdownMenuItem>
-    //                 View
-    //             </DropdownMenuItem>
-    //         </Link>
-    //         <DropdownMenuItem>
-    //             Delete
-    //             <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
-    //         </DropdownMenuItem>
-    //     </DropdownMenuContent>
-    // </DropdownMenu>
+        <div className="text-right">
+            <Button variant="outline" size="icon" onClick={() => router.push(`/contacts/${id}`)}>
+                <ChevronRight />
+            </Button>
+        </div>
     )
 }
