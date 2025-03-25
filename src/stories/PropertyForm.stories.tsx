6e99ad0ab9baa2b0a18f6e9675031f68949e438a
Jhoseph <jhoseph.dev@gmail.com>
2025-03-25 22:00:13 +0100

    Add Storybook story for PropertyForm
    Create a Storybook configuration for the PropertyForm component, including a default story with mock data and hooks. This facilitates UI testing and development for the PropertyForm component.

---
+++ src/stories/PropertyForm.stories.tsx
@@ -0,0 +1,57 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { PropertyForm } from "@/app/(app)/properties/components/PropertyForm";
+import {usePropertyForm} from "@/hooks/property/usePropertyForm";
+import React from "react";
+
+const data = {
+    id: 1,
+    reference: '',
+    type: "flat",
+    state: "new",
+    status: "available",
+    contact_id: 0,
+    user_id: 1,
+    latitude: 0,
+    longitude: 0,
+    image: [],
+    floor: "0",
+    door: "0"
+}
+
+const meta: Meta<typeof PropertyForm> = {
+    parameters: {
+      nextjs: {
+          appDirectory: true,
+      }
+    },
+    title: "Organisms/PropertyForm",
+    component: PropertyForm,
+    args: {
+        handleSubmit: async (values) => {
+            console.log(values);
+        },
+        handleDelete: async () => {
+            console.log('paz');
+        },
+        isSubmitting: false,
+        data,
+        isNew: false,
+        rechargeFunctionProperty: async () => {
+            console.log('paz');
+        },
+        setIsEditing: (mode: "edit" | "view") => {
+            console.log(mode);
+        },
+    },
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof PropertyForm> = {
+    render: (args) => {
+        const methods = usePropertyForm(data)
+        return (
+            <PropertyForm {...args}  methods={methods}/>
+        );
+    },
+};
